225bd3514453dba3137bb37153b6f3e2bb05cbbb
mrparkersson <amoparkerprince78@gmail.com>
2022-10-03 12:03:05 +0000

    Add react native navigation package

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import SigninScreen from './src/screens/SigninScreen';
+import SignupScreen from './src/screens/SignupScreen';
+import ConfirmEmailScreen from './src/screens/ConfirmEmailScreen';
+import ForgotPasswordScreen from './src/screens/ForgotPasswordScreen';
+import NewPasswordScreen from './src/screens/NewPasswordScreen';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.root}>
-      <SigninScreen />
+      {/* <SigninScreen /> */}
+      {/* <SignupScreen /> */}
+      {/* <ConfirmEmailScreen /> */}
+      {/* <ForgotPasswordScreen /> */}
+      <NewPasswordScreen />
     </SafeAreaView>
   );
 }
